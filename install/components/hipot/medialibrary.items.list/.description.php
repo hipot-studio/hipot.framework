a667a0192f294b327608a91208e6ca1ce27b56ac
hipot <hipot@ya.ru>
2019-11-14 01:54:46 +0200

    last changes (medialibrary.items.list)

---
+++ install/components/hipot/medialibrary.items.list/.description.php
@@ -0,0 +1,39 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+/**
+ *
+ * Компонент "Список элементов медиабиблиотеки"
+ * medialibrary.items.list
+ *
+ * @version 2.0, 12.08.2017
+ * @author hipot
+ *
+ */
+
+
+/**
+ * Параметры компонента (пока 3 штуки):
+ *
+ * COLLECTION_IDS			- числовые идентификаторы коллекций (либо один числовой идентификатор)
+ * CACHE_TIME				- время кеша
+ * ONLY_RETURN_ITEMS 		- Y/N, по-умолчанию N.
+ * 		параметр, используется компонентом medialibrary.collection.list
+ * 		если установлено в Y, то кеш отключается, а компонент не подключая шаблон возвращает
+ * 		свой массив элементов (т.е. компонент возвращает свой $arResult)
+ *
+ */
+
+$arComponentDescription = array(
+	"NAME"			=> "medialibrary.items.list",
+	"DESCRIPTION"	=> "",
+	"ICON"			=> "/images/ico.gif",
+	"PATH" => array(
+		"ID"		=> "hipot_root",
+		"NAME"		=> "hipot"
+	),
+	"AREA_BUTTONS"	=> array(),
+	"CACHE_PATH"	=> "Y",
+	"COMPLEX"		=> "N"
+);
+?>
