13fb0017a584a07001603651b1e7f8b95fb34d84
hipot <hipot@ya.ru>
2021-06-22 00:10:32 +0300

    last changes

--- install/components/hipot/custom.order/.description.php
+++
@@ -1,8 +0,0 @@
-<?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-$arComponentDescription = array(
-	"NAME" => GetMessage("CUSTOM_ORDER_TEMPLATE_NAME"),
-	"DESCRIPTION" => GetMessage("CUSTOM_ORDER_TEMPLATE_DESCRIPTION"),
-);
-?>
