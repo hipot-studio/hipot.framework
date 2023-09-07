286379c25b327db3f6ed75cd75c751aea1927a55
hipot <hipot@ya.ru>
2023-09-07 22:15:50 +0300

    function autoload by composer.json

--- include.php
+++ include.php
@@ -4,8 +4,15 @@ defined('B_PROLOG_INCLUDED') || die();
  * Bitrix init.php entry point example
  */
 
+// region better use composer 'vendor/autoload.php'
+
 // autoloader (deprecated, better use composer PSR-4 autoloader)
 require __DIR__ . '/lib/simple_loader.php';
+
+// плавающие функции
+require __DIR__ . '/lib/functions.php';
+
+// endregion
 
 // iblock props in memcache
 if (file_exists(__DIR__ . '/lib/ib_props_memcache.php')) {
@@ -16,9 +23,6 @@ if (file_exists(__DIR__ . '/lib/ib_props_memcache.php')) {
 if (file_exists(__DIR__ . '/lib/iblock_layer_model.php')) {
 	require __DIR__ . '/lib/iblock_layer_model.php';
 }
-
-// плавающие функции
-require __DIR__ . '/lib/functions.php';
 
 // добавление обработчиков (без определения, определения писать лучше в отдельном классе)
 require __DIR__ . '/lib/handlers_add.php';
