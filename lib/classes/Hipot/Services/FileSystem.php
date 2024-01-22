7ab3d3b055c7c778174399777979a9361ccd9f0b
hipot <hipot@ya.ru>
2024-01-22 21:31:42 +0200

    Add filePrepend method to FileSystem class
    A new method, 'filePutPrepend', has been added to the FileSystem class in the Hipot Services. This method allows for prepending content to a file, enhancing the file handling capabilities of the application.

--- lib/classes/Hipot/Services/FileSystem.php
+++ lib/classes/Hipot/Services/FileSystem.php
@@ -36,4 +36,34 @@ class FileSystem
 			yield $file;
 		}
 	}
+
+	/**
+	 * Prepend content to a file.
+	 *
+	 * @param string $strFilename The path to the file to prepend to.
+	 * @param string $strPrepend The content to prepend. It can be of any type.
+	 *
+	 * @return void
+	 */
+	public static function filePutPrepend(string $strFilename = '', string $strPrepend = ''): void
+	{
+		$handler = fopen($strFilename, 'rb+');
+		flock($handler, LOCK_EX);
+		rewind($handler);
+
+		$prepend = $strPrepend;
+		$chunkLength = mb_strlen($prepend);
+		$i = 0;
+		do {
+			$readData = fread($handler, $chunkLength);
+			fseek($handler, $i * $chunkLength);
+			fwrite($handler, $prepend);
+
+			$prepend = $readData;
+			$i++;
+		} while ($readData);
+
+		flock($handler,  LOCK_UN);
+		fclose($handler);
+	}
 }
