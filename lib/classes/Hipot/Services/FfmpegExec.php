143475c3a1c2b28c2cd714e348ea788ecef846bd
hipot <hipot@ya.ru>
2024-01-29 20:48:49 +0200

    Add FfmpegExec class to handle video resizing
    Created a new FfmpegExec class in the Hipot\Services namespace that includes methods for retrieving video size and resizing videos. The class uses ffmpeg and ffprobe utilities, providing a handy way to integrate video manipulations into projects. It inherently supports resizing by height with autofill of width parameter.

---
+++ lib/classes/Hipot/Services/FfmpegExec.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * hipot studio source file <info AT hipot-studio DOT com>
+ * Created 21.03.2023 18:24
+ * @version pre 1.0
+ */
+namespace Hipot\Services;
+
+use Bitrix\Main\Web\Json;
+use Hipot\Utils\UUtils;
+
+/**
+ * yum install ffmpeg
+ */
+final class FfmpegExec
+{
+	/**
+	 * 1080p (HD): 1920 x 1080
+	 * 720p (HD): 1280 x 720
+	 * 480p (SD): 854 x 480
+	 */
+	public const SIZES_H = [
+		'480p' => 480,
+		'720p' => 720,
+		'1080p' => 1080
+	];
+
+	/**
+	 * set -1 in width or height to autofill
+	 */
+	private const RESIZE_SIZE_BY_HEIGHT_SH = 'ffmpeg -v quiet -i #SRC_FILENAME# -filter:v scale="#WIDTH#:#HEIGHT#" -c:a copy #DEST_FILENAME#';
+	private const GET_SIZE_SH = 'ffprobe -v quiet -show_entries stream=width,height,codec_type -select_streams v:0 -of json #FILENAME#';
+
+	/**
+	 * @return array{'width':int, 'height':int}
+	 */
+	public static function getVideoSize(string $filename): array
+	{
+		$sizes = ['width' => 0, 'height' => 0];
+
+		if (!is_file($filename)) {
+			return $sizes;
+		}
+
+		try {
+			@exec( str_replace('#FILENAME#', $filename, self::GET_SIZE_SH), $cmdResult );
+			$result = Json::decode( implode('', $cmdResult) );
+			$sizes = current($result['streams']);
+		} catch (\Error|\Exception $e) {
+			UUtils::logException($e);
+		}
+
+		return $sizes;
+	}
+
+	public static function resizeVideo(string $srcFilename, string $destFilename, array $sizes): bool
+	{
+		if (is_file($destFilename)) {
+			return true;
+		}
+		if (! is_file($srcFilename)) {
+			return false;
+		}
+		if (!mkdir($concurrentDirectory = dirname($destFilename), constant('BX_DIR_PERMISSIONS'), true) && !is_dir($concurrentDirectory)) {
+			return false;
+		}
+
+		$cmd = str_replace([
+				'#WIDTH#', '#HEIGHT#', '#SRC_FILENAME#', '#DEST_FILENAME#'
+			], [
+				$sizes['width'], $sizes['height'], $srcFilename, $destFilename
+			],
+			self::RESIZE_SIZE_BY_HEIGHT_SH
+		);
+		@exec($cmd, $cmdResult);
+
+		return is_file($destFilename);
+	}
+}
