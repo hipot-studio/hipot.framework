751e6747890fa7aeb626249287fba8981b35ef99
hipot <hipot@ya.ru>
2025-05-15 20:30:34 +0300

    - Add class documentation for `Includer`
    Added a PHPDoc block to the `Includer` class explaining its purpose, usage, and example code snippets.

--- install/components/hipot/includer/class.php
+++ install/components/hipot/includer/class.php
@@ -1,10 +1,28 @@
 <?php
 namespace Hipot\Components;
 
+/**
+ * Class Includer
+ *
+ * This class is an extension of the CBitrixComponent class
+ * and is responsible for executing the component logic, including
+ * caching and template inclusion.
+ * <code>
+ * $includer = $APPLICATION->IncludeComponent('hipot:includer', 'some.page', []);
+ * echo $includer->arResult['SOME_TEMPLATE_PAGE_PARAMS'];
+ * unset($includer);
+ * // or
+ * $APPLICATION->IncludeComponent('hipot:includer', 'some.widget', []);
+ * </code>
+ */
 class Includer extends \CBitrixComponent
 {
 	public function executeComponent()
 	{
-		$this->includeComponentTemplate();
+		if ($this->startResultCache(false)) {
+			$this->setResultCacheKeys([]);
+			$this->includeComponentTemplate();
+		}
+		return $this;
 	}
 }
