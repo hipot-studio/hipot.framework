b9f9d1d3a1f2006b641caba97f94229ddd9c5d01
Sasha Manoil <manoil.alexandr96@gmail.com>
2025-11-14 14:04:12 +0200

    Описан пример использования компонента iblock_like

--- install/components/hipot/ajax/templates/iblock_like/script.js
+++ install/components/hipot/ajax/templates/iblock_like/script.js
@@ -1,69 +1,101 @@
-$(function (){
-	// load interface
-	let likeIds = [], lang, type;
-	$("[data-like-block]").each(function (){
-		likeIds[likeIds.length] = $(this).data('id');
-		if (typeof lang === 'undefined') {
-			lang = $(this).data('lang')
-		}
-		if (typeof type === 'undefined') {
-			type = $(this).data('type')
-		}
-	});
-	BX.ajax.runComponentAction('hipot:ajax', 'loadIblockLikeTemplates', {
-		mode: 'class',
-		data: {
-			'ids'   : likeIds,
-			'type'  : type,
-			'lang'  : lang,
-		},
-		method: 'POST'
-	}).then(function(response){
-		if (response.status === 'success') {
-			$("[data-like-block]").each(function (){
-				let _block = $(this),
-					id = $(_block).data('id');
-				if (typeof response.data[id] !== 'undefined') {
-					_block.replaceWith(response.data[id].HTML);
+$(function () {
+    // load interface
+    let likeIds = [],
+        lang,
+        type,
+        template;
 
-					if (BX.localStorage.get('like-pt-' + id) != 1) {
-						$(".ico-like-pt[data-id="+ id +"]").fadeTo(0, 0.5);
-					}
+		$("[data-like-block]").each(function () {
+			likeIds[likeIds.length] = $(this).data("id");
+			template = $(this).data("template");
+			if (typeof lang === "undefined") {
+				lang = $(this).data("lang");
+			}
+			if (typeof type === "undefined") {
+				type = $(this).data("type");
+			}
+		});
+
+		BX.ajax
+			.runComponentAction("hipot:ajax", "loadIblockLikeTemplates", {
+				mode: "class",
+				data: {
+					ids: likeIds,
+					type: type,
+					lang: lang,
+					template: template,
+				},
+				method: "POST",
+			})
+			.then(function (response) {
+				if (response.status === "success") {
+					$("[data-like-block]").each(function () {
+						let _block = $(this),
+							id = $(_block).data("id");
+						if (typeof response.data[id] !== "undefined") {
+							_block.replaceWith(response.data[id].HTML);
+
+							if (BX.localStorage.get("like-pt-" + id) == 1) {
+								$(`.blog-section_inner_grid_item_info_favorite[data-id="${id}"]`).addClass("blog-section_inner_grid_item_info_favorite--added");
+							}
+						}
+					});
+				} else {
+					console.log(response);
 				}
 			});
-		} else {
-			console.log(response);
-		}
-	});
 
-	// actions on interface
-	$(document).on('click', '.ico-like-pt', function (){
-		let _block = $(this),
-			id = $(_block).data('id'),
-			type = $(_block).data('type'),
-			lang = $(_block).data('lang');
+    // actions on interface
+    $(document).on("click", ".blog-section_inner_grid_item_info_favorite", function () {
+            let _block = $(this),
+                id = $(_block).data("id"),
+                type = $(_block).data("type"),
+                lang = $(_block).data("lang"),
+                isLiked = BX.localStorage.get("like-pt-" + id) == 1;
 
-		if (_block.data('clicked') || BX.localStorage.get('like-pt-' + id) == 1) {
-			return;
-		}
-		_block.fadeTo(0, 0.5).data('clicked', true);
-		BX.localStorage.set('like-pt-' + id, 1, 3600 * 24 * 356);
+            const likeAction = isLiked ? "-" : "+";
 
-		BX.ajax.runComponentAction('mgu:ajax', 'saveIblockLike', {
-			mode: 'class',
-			data: {
-				'id'    : id,
-				'type'  : type,
-				'lang'  : lang,
-			},
-			method: 'POST'
-		}).then(function(response){
-			if (response.status === 'success') {
-				$("[data-id-num-likes=" + id + '] b').html(response.data.CNT_P);
-			} else {
-				console.log(response);
-			}
-			_block.fadeTo(0, 1);
-		});
-	});
+            if (isLiked) {
+                _block.removeClass("blog-section_inner_grid_item_info_favorite--added");
+                like({ id, type, lang, value: likeAction }, _block);
+                BX.localStorage.remove("like-pt-" + id, null);
+
+                return;
+            }
+
+            _block.addClass("blog-section_inner_grid_item_info_favorite--added");
+            like({ id, type, lang, value: likeAction }, _block);
+            BX.localStorage.set("like-pt-" + id, 1, 3600 * 24 * 356);
+        }
+    );
+
+    /**
+     * @typedef {Object} LikeActionData
+     * @property {number} id
+     * @property {string} type
+     * @property {string} lang
+     * @property {string} value - property defines which action we're doing. "+" = like, "-" = dislike
+     */
+
+    /**
+     * Like action for iblock_like
+     * @param {LikeActionData} data - for like action
+     */
+    function like(data) {
+        BX.ajax
+            .runComponentAction("hipot:ajax", "saveIblockLike", {
+                mode: "class",
+                data,
+                method: "POST",
+            })
+            .then(function (response) {
+                const { id } = data;
+
+                if (response.status === "success") {
+                    $(`[data-id-num-likes="${id}"]`).html(response.data.CNT_P);
+                } else {
+                    console.log(response);
+                }
+            });
+    }
 });
