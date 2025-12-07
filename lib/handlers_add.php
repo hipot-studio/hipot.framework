ab1ca2cf24e68a4f0c51ff7224dfbe821a7c32d9
hipot <info@hipot-studio.com>
2025-12-07 17:35:37 +0200

    Add meta-data to property names in admin panel
    - Display property ID and CODE for certain IBlock properties on the admin interface, improving visibility for beta testers.
    - Includes conditional CSS injection based on request parameters.

--- lib/handlers_add.php
+++ lib/handlers_add.php
@@ -71,19 +71,19 @@ $eventManager->addEventHandler("main", "OnBeforeProlog", static function () use 
 	BitrixEngine::resetInstance();
 
 	foreach (
-			[
-					__DIR__ . '/constants.php',
-					__DIR__ . '/lib/constants.php',     // handler in init.php
-					Loader::getDocumentRoot() . '/local/php_interface/include/constants.php',
-					Loader::getDocumentRoot() . '/local/php_interface/include/lib/constants.php',
-					Loader::getDocumentRoot() . '/bitrix/php_interface/include/constants.php',
-					Loader::getDocumentRoot() . '/bitrix/php_interface/include/lib/constants.php'
-			] as $constFile) {
-		if (is_file($constFile)) {
-			include $constFile;
-			break;
+		[
+			__DIR__ . '/constants.php',
+			__DIR__ . '/lib/constants.php',     // handler in init.php
+			Loader::getDocumentRoot() . '/local/php_interface/include/constants.php',
+			Loader::getDocumentRoot() . '/local/php_interface/include/lib/constants.php',
+			Loader::getDocumentRoot() . '/bitrix/php_interface/include/constants.php',
+			Loader::getDocumentRoot() . '/bitrix/php_interface/include/lib/constants.php'
+		] as $constFile) {
+			if (is_file($constFile)) {
+				include $constFile;
+				break;
+			}
 		}
-	}
 
 	// create user-d0 to work in agents
 	if ($USER === null) {
@@ -107,6 +107,50 @@ $eventManager->addEventHandler("main", "OnBuildGlobalMenu", static function (&$a
 	}
 });
 
+// Add meta-data to property names (ID and CODE), example: Article: 123, CML2_ARTICLE
+$eventManager->addEventHandler('main', 'OnEndBufferContent', static function (&$strContent) use ($request) {
+	if (!defined('IS_BETA_TESTER') || !IS_BETA_TESTER) {
+		return;
+	}
+	$arAllowedUrls = ['/bitrix/admin/cat_product_edit.php', '/bitrix/admin/iblock_element_edit.php'];
+	$intIBlockId = (int)$request->getQueryList()->get('IBLOCK_ID');
+	
+	if ($intIBlockId <= 0 || !in_array(BitrixEngine::getAppD0()->GetCurPage(false), $arAllowedUrls, false)) {
+		return;
+	}
+	
+	$arProps  = [];
+	$resProps = \CIBlockProperty::getList([], ['IBLOCK_ID' => $intIBlockId]);
+	while ($arProp = $resProps->fetch()) {
+		$arProps[$arProp['ID']] = $arProp;
+	}
+	unset($resProps);
+	/** @noinspection HtmlUnknownAttribute */
+	$strRegExp  = '#(<tr [^>]*id="tr_PROPERTY_(\d+)"[^>]*>\s*<td class="[^>]+" width="40%">)(\s*<span[^>]+>.*?</script>&nbsp;)?(\s*[^>]+:\s*)(</td>)#is';
+	$strContent = preg_replace_callback($strRegExp, static function ($arMatch) use ($arProps) {
+		$strName = sprintf('%s%s <span class="hi_iblock_prop_meta">%d, %s</span>', $arMatch[3], $arMatch[4],
+			$arMatch[2], $arProps[$arMatch[2]]['CODE']);
+		return $arMatch[1] . $strName . $arMatch[5];
+	}, $strContent);
+	//
+	$strCss = '
+		<style>
+			.hi_iblock_prop_meta {
+				color:#999 !important;
+				display:block !important;
+				font-size:11px !important;
+				padding-right:3px !important;
+				text-align:right !important;
+			}
+		</style>
+		';
+	if ($request->getQueryList()->get('bxpublic') == 'Y') {
+		$strContent = $strCss . $strContent;
+	} else {
+		$strContent = str_ireplace('</head>', $strCss . '</head>', $strContent);
+	}
+});
+
 // RemoveYandexDirectTab in iblock elements
 $eventManager->addEventHandler('main', 'OnAdminTabControlBegin', static function (&$TabControl) {
 	/** @var $GLOBALS array{'DB':\CDatabase, 'APPLICATION':\CMain, 'USER':\CUser, 'USER_FIELD_MANAGER':\CUserTypeManager, 'CACHE_MANAGER':\CCacheManager, 'stackCacheManager':\CStackCacheManager} */
@@ -126,10 +170,10 @@ $eventManager->addEventHandler(	"main", "OnAdminListDisplay",
 		if ($this_al->table_id == "tbl_user" || str_contains($this_al->table_id, 'iblock') || str_starts_with($this_al->table_id, 'tbl_hi')) {
 			echo $this_al->sNavText;
 			BitrixEngine::getInstance()->asset->addString('
-			<style>
-				.adm-workarea > .main-ui-pagination {padding:10px 0;}
-			</style>
-		');
+				<style>
+					.adm-workarea > .main-ui-pagination {padding:10px 0;}
+				</style>
+			');
 		}
 		if ($this_al->table_id == "tbl_user") {
 			foreach ($this_al->aRows as &$row) {
@@ -137,7 +181,7 @@ $eventManager->addEventHandler(	"main", "OnAdminListDisplay",
 				$picPath = CFile::GetPath( (CUser::GetByID($userId)->Fetch())["PERSONAL_PHOTO"] );
 				if (trim($picPath) != '') {
 					$row->aFields["LOGIN"]["view"]["value"] .= ' <br><a target="_blank" href="' . $picPath . '">'
-							. '<img style="max-width:200px;" alt="" loading="lazy" src="' . $picPath  . '"></a>';
+						. '<img style="max-width:200px;" alt="" loading="lazy" src="' . $picPath  . '"></a>';
 				}
 			}
 		}
@@ -197,8 +241,8 @@ $eventManager->addEventHandler('main', 'OnBeforeEndBufferContent', static functi
 
 	$pCfg = array_shift($p);
 	if ($APPLICATION->GetCurPage() != '/bitrix/admin/user_options.php'
-			|| $pCfg['c'] != 'form' || $pCfg['d'] != 'Y'
-			|| !preg_match('#^form_((section)|(element))_\d+$#', $pCfg['n'])
+		|| $pCfg['c'] != 'form' || $pCfg['d'] != 'Y'
+		|| !preg_match('#^form_((section)|(element))_\d+$#', $pCfg['n'])
 	) {
 		return;
 	}
@@ -217,18 +261,18 @@ $eventManager->addEventHandler('main', 'OnEndBufferContent', static function (&$
 
 	if (is_object($USER) && !$USER->IsAuthorized() && !$request->isPost() && $APPLICATION->GetProperty('JS_core_frame_cache_NEED') != 'Y') {
 		$toRemove = [
-				'#<script[^>]+src="/bitrix/js/ui/dexie/[^>]+></script>#',
-				'#<script[^>]+src="/bitrix/js/main/core/core_frame_cache[^>]+></script>#',
-				'#<link[^>]+href="/bitrix/js/ui/design-tokens/dist/ui.design-tokens.min.css\?\d+"[^>]+>#',
-				'#<link[^>]+href="/bitrix/panel/main/popup.min.css\?\d+"[^>]+>#',
+			'#<script[^>]+src="/bitrix/js/ui/dexie/[^>]+></script>#',
+			'#<script[^>]+src="/bitrix/js/main/core/core_frame_cache[^>]+></script>#',
+			'#<link[^>]+href="/bitrix/js/ui/design-tokens/dist/ui.design-tokens.min.css\?\d+"[^>]+>#',
+			'#<link[^>]+href="/bitrix/panel/main/popup.min.css\?\d+"[^>]+>#',
 		];
 		$cont = preg_replace($toRemove, "", $cont);
 	}
 
 	$toInline = [
-			'#<script[^>]+src="(?<src>/bitrix/js/main/core/core.min.js)\?\d+"[^>]*></script>#',
-			'#<script[^>]+src="(?<src>/bitrix/js/main/core/core_ls.min.js)\?\d+"[^>]*></script>#',
-			'#<script[^>]+src="(?<src>/bitrix/cache/js/.*?\.js)\?\d+"[^>]*></script>#'
+		'#<script[^>]+src="(?<src>/bitrix/js/main/core/core.min.js)\?\d+"[^>]*></script>#',
+		'#<script[^>]+src="(?<src>/bitrix/js/main/core/core_ls.min.js)\?\d+"[^>]*></script>#',
+		'#<script[^>]+src="(?<src>/bitrix/cache/js/.*?\.js)\?\d+"[^>]*></script>#'
 	];
 	$cont = preg_replace_callback($toInline, static function ($matches) {
 		$content = file_get_contents(Loader::getDocumentRoot() . $matches['src']);
@@ -255,10 +299,10 @@ $eventManager->addEventHandler('catalog', 'OnGetDiscountResult', static function
 		UUtils::setPrivateProperty(\CAllCatalogDiscount::class, 'arCacheProduct', []);
 
 		\CCatalogDiscount::ClearDiscountCache([
-				'PRODUCT' => true,
-				/*'SECTIONS'        => true,
-				'PROPERTIES'        => true,
-				'SECTION_CHAINS'    => true*/
+			'PRODUCT' => true,
+			/*'SECTIONS'        => true,
+			'PROPERTIES'        => true,
+			'SECTION_CHAINS'    => true*/
 		]);
 	}
 	return true;
@@ -274,19 +318,19 @@ $eventManager->addEventHandler("sale", "OnBasketAdd", static function ($ID, $arF
 	Hipot\BitrixUtils\Sale::deleteUnUsedBasketProps($ID);
 });
 $eventManager->addEventHandler(
-		'sale',
-		'OnSaleOrderSaved',
-		static function (Event $event) {
-			/** @var \Bitrix\Sale\Order $order */
-			$order = $event->getParameter("ENTITY");
+	'sale',
+	'OnSaleOrderSaved',
+	static function (Event $event) {
+		/** @var \Bitrix\Sale\Order $order */
+		$order = $event->getParameter("ENTITY");
 
-			if (isset($GLOBALS['deletedOrderUnusedProps_' . $order->getId()])) {
-				return;
-			}
-
-			Hipot\BitrixUtils\Sale::deleteUnUsedBasketProps();
-			$GLOBALS['deletedOrderUnusedProps_' . $order->getId()] = true;
+		if (isset($GLOBALS['deletedOrderUnusedProps_' . $order->getId()])) {
+			return;
 		}
+
+		Hipot\BitrixUtils\Sale::deleteUnUsedBasketProps();
+		$GLOBALS['deletedOrderUnusedProps_' . $order->getId()] = true;
+	}
 );
 // endregion
 
