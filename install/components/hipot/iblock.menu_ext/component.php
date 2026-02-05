c7614a91a960a32d7ad1260b612940846ce44945
hipot <info@hipot-studio.com>
2026-02-05 14:01:12 +0200

    Refine iblock.menu_ext component parameter handling
    - Simplified `defined` check for `B_PROLOG_INCLUDED`.
    - Ensured required array parameters default to empty arrays.
    - Updated cache path generation to use `mb_strtolower` for better compatibility.
    - Added stricter checks for array-type parameters in menu generation logic.
    - Included `unset` for temporary variables to clean up the scope.

--- install/components/hipot/iblock.menu_ext/component.php
+++ install/components/hipot/iblock.menu_ext/component.php
@@ -1,4 +1,4 @@
-<?php if (! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+<?php defined('B_PROLOG_INCLUDED') || die();
 
 use Hipot\BitrixUtils\PhpCacher;
 use function Opis\Closure\unserialize as UnserializeClosure; // from 4.4 version of a library
@@ -14,11 +14,18 @@ use function Opis\Closure\unserialize as UnserializeClosure; // from 4.4 version
 
 $reqParams = ['TYPE', 'CACHE_TAG', 'CACHE_TIME'];
 foreach ($reqParams as $param) {
-	if (trim($arParams[ $param ]) == '') {
+	if (trim($arParams[$param]) == '') {
 		ShowError('Need PARAM ' . $param . ', see .description.php!');
 		return false;
 	}
 }
+$needArrayParams = ['ORDER', 'FILTER', 'SELECT'];
+foreach ($needArrayParams as $param) {
+	if (!is_array($arParams[$param])) {
+		$arParams[$param] = [];
+	}
+}
+unset($reqParams, $needArrayParams);
 
 // сюда соберем все пункты меню
 $arResult 		= [];
@@ -27,7 +34,7 @@ $CACHE_TIME		= (COption::GetOptionString("main", "component_cache_on", "Y") == "
 	? 0
 	: (int)$arParams['CACHE_TIME'];
 $CACHE_ID		= __FILE__ . '|' . serialize($arParams);
-$cachePath		= 'php/' . ToLower($arParams['CACHE_TAG']) . '/';
+$cachePath		= 'php/' . mb_strtolower($arParams['CACHE_TAG']) . '/';
 
 $obMenuCache = new CPHPCache();
 PhpCacher::noOutputCacheD0($obMenuCache);
@@ -75,7 +82,7 @@ if ($obMenuCache->StartDataCache($CACHE_TIME, $CACHE_ID, $cachePath)) {
 	} else if ($arParams["TYPE"] == 'sections') {
 		
 		$arSelect = ['ID', 'IBLOCK_ID', 'CODE', 'SECTION_PAGE_URL', 'NAME', 'DEPTH_LEVEL'];
-		if (count($arParams['SELECT']) > 0) {
+		if (is_array($arParams["SELECT"]) && count($arParams['SELECT']) > 0) {
 			$arSelect = array_merge($arSelect, $arParams['SELECT']);
 		}
 		
@@ -83,7 +90,7 @@ if ($obMenuCache->StartDataCache($CACHE_TIME, $CACHE_ID, $cachePath)) {
 		
 		$rsSect = CIBlockSection::GetList($arOrder, $arFilter, false, $arSelect, $arNavStartParams);
 		while ($arSect = $rsSect->GetNext()) {
-			$link_params = (count($arParams["SELECT"]) > 0) ? $arSect : [];
+			$link_params = (is_array($arParams["SELECT"]) && count($arParams["SELECT"]) > 0) ? $arSect : [];
 			$addonsUri = !empty($arParams['ADDON_URL_TO_SELECT_ITEM']) ? [CIBlock::ReplaceSectionUrl($arParams['ADDON_URL_TO_SELECT_ITEM'], $arSect)] : [];
 			
 			if (!empty($arParams['~MODIFY_ITEM'])) {
