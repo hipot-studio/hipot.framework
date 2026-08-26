5b930ebb8c31f3cad5b2a34173f2fa56dff4c200
hipot <info@hipot-studio.com>
2026-08-26 15:48:45 +0300

    Refine error page email handling logic
    - Extract primary email address from `ERROR_EMAIL` using `explode` for `mailto` links.
    - Adjust variable names for clarity and consistency.

--- install/pages/error.php
+++ install/pages/error.php
@@ -26,8 +26,9 @@ use Bitrix\Main\Diag\ExceptionHandlerFormatter,
 	Bitrix\Main\Mail\Event,
 	Bitrix\Main\SiteTable;
 
-$developerEmail = defined('ERROR_EMAIL') ? ERROR_EMAIL : 'info@hipot-studio.com';
-$request        = Application::getInstance()?->getContext()?->getRequest();
+$developerEmail       = defined('ERROR_EMAIL') ? ERROR_EMAIL : 'info@hipot-studio.com';
+$developerEmailMailTo = trim(explode(',', $developerEmail)[0]);
+$request              = Application::getInstance()?->getContext()?->getRequest();
 
 // to copy and one-time-run in the admin PHP Command line instrument...
 $installEmailType       = static function ($typeId = 'DEBUG_MESSAGE'): bool {
@@ -203,7 +204,7 @@ if ($isAjaxRequest) {
 		<div class="we-know">Мы уже об этом знаем, и в ближайшее время все будет исправлено.</div>
 		Просим прощения за неудобства.<br /><br />
 
-		<small>Вы можете <a href="mailto:<?=$developerEmail?>?subject=Ошибка+PHP+<?=htmlspecialcharsbx($request?->getServer()->getServerName() . $request?->getRequestUri())?>">написать нам</a>, указав подробности,
+		<small>Вы можете <a href="mailto:<?=$developerEmailMailTo?>?subject=Ошибка+PHP+<?=htmlspecialcharsbx($request?->getServer()->getServerName() . $request?->getRequestUri())?>">написать нам</a>, указав подробности,
 			если вы не впервые видите данную ошибку</small><br />
 		
 		<?
