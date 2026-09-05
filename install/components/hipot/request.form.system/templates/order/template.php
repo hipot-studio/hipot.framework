ee94e5c28c31ef314844549d3424d2d86b695863
hipot <info@hipot-studio.com>
2026-09-05 16:28:30 +0300

    Add template metadata and frame mode support
    - Added template metadata comments to define variables and improve readability in several template files.
    - Enabled `$this->setFrameMode(true)` for better component performance and security.

--- install/components/hipot/request.form.system/templates/order/template.php
+++ install/components/hipot/request.form.system/templates/order/template.php
@@ -1,4 +1,23 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+// region var_templ
+/**
+ * @var array $arParams
+ * @var array $arResult
+ * @global CMain $APPLICATION
+ * @global CUser $USER
+ * @global CDatabase $DB
+ * @var CBitrixComponentTemplate $this
+ * @var string $componentPath
+ * @var string $templateName
+ * @var string $templateFile
+ * @var string $templateFolder
+ * @var array $templateData
+ * @var CBitrixComponent $component
+ */
+// endregion
+$this->setFrameMode(true);
+?>
 
 <div class="applicat static_form" id="<?=$arParams['POST_NAME']?>_form">
 
--- install/components/hipot/request.form.system/templates/recall_popup_ajax/template.php
+++ install/components/hipot/request.form.system/templates/recall_popup_ajax/template.php
@@ -1,5 +1,23 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
+// region var_templ
+/**
+ * @var array $arParams
+ * @var array $arResult
+ * @global CMain $APPLICATION
+ * @global CUser $USER
+ * @global CDatabase $DB
+ * @var CBitrixComponentTemplate $this
+ * @var string $componentPath
+ * @var string $templateName
+ * @var string $templateFile
+ * @var string $templateFolder
+ * @var array $templateData
+ * @var CBitrixComponent $component
+ */
+// endregion
+$this->setFrameMode(true);
+?>
 
 <?// эти две дивки вместо тега form ?>
 <div class="ajax_form popup" id="<?=$arParams['POST_NAME']?>_form">
