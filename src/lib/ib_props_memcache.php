ac6758270f34c0d90baa337a3e7d01e052d136b4
hipot <info@hipot-studio.com>
2026-09-10 18:35:44 +0300

    Update and optimize ib_props_memcache.php logic
    - Removed deprecated and overly verbose comments.
    - Updated caching logic for CCatalogSku with managed cache improvements.
    - Enhanced property handling via Memcache for \CIBlockProperty and \CIblockElement methods.
    - Refactored and streamlined namespace and cache configuration.
    - Improved code alignment and readability for better maintainability.

--- src/lib/ib_props_memcache.php
+++ src/lib/ib_props_memcache.php
@@ -1,75 +1,9 @@
 <?php
 /**
- * @deprecated from iblock 23.200.0
- * https://dev.1c-bitrix.ru/docs/versions.php?lang=ru&module=iblock
- */
-
-/**
- * @see http://dev.1c-bitrix.ru/community/webdev/user/17890/blog/8910/?commentId=49110#com49110
- *
- * в Битриксе есть метод CIBlockProperty::GetPropertyArray. Это служебный метод, который принимает в качестве параметров
- * идентификаторы информационного блока и свойства и возвращает информацию об указанном свойстве из таблицы b_iblock_property.
- *
- * Характерная особенность этого метода заключается в том, что он вызывается очень часто. Практически любое действие,
- * которое хоть как-то затрагивает свойства инфоблока приводит к вызову этого метода и запросу к базе данных.
- *
- * Для большинства проектов это не является проблемой: у таблицы b_iblock_property есть все необходимые индексы
- * и запросы получаются очень быстрыми. Однако, когда на проекте заведены сотни информационных блоков,
- * а посещаемость измеряется миллионами хитов в сутки, начинает сказываться количество этих запросов.
- * И не просто сказываться — база начинает задыхаться.
- *
- * При этом, в коде метода нетрудно заметить так называемый "виртуальный кеш": полученные данные сохраняются в
- * глобальном массиве $IBLOCK_CACHE_PROPERTY и при следующем вызове метода для того же
- * свойства, данные возвращаются уже не из базы, а из этого глобального массива.
- *
- * Проблема заключается в том, что время жизни "виртуального кеша" не превышает
- * времени жизни скрипта — долей секунды. При этом, обновление записей в
- * таблице b_iblock_property происходит очень-очень редко.
- *
- * Отсюда возникает очевидная задача: требуется продлить время жизни "виртуального кеша", не затрагивая при этом код ядра Битрикс.
- *
- * Решение следующее:
- *  * Разрабатывается класс, реализующий интерфейс ArrayAccess. В классе обеспечивается работа с подходящим
- *  in-memory key-value store (Memcached, Redis или любой другой). В случае, если сервис недоступен, объект ведет себя как обычный массив.
- *
- *  * В php_interface/init.php производится подключение модуля информационных блоков и создается объект CIBlockProperty.
- *  Создание объекта приводит к однократной инициализации глобального массива $IBLOCK_CACHE_PROPERTY. После этого созданный объект можно уничтожить, выполнив unset.
- *
- *  * В php_interface/init.php глобальный массив $IBLOCK_CACHE_PROPERTY переопределяется объектом класса,
- *  созданного на этапе 1. Переопределение производится ниже места, описанного на этапе 2.
- *
- *
- * Задача решена: теперь данные кешируются в быстром key-value store,
- * а при вызове метода GetPropertyArray обращения к базе данных больше не происходит.
- * На реализацию потрачен час времени, добавлено около 50 строк понятного кода, ядро Битрикс осталось нетронутым.
- *
- * К сожалению, я не имею права показывать графики. Могу лишь сказать, что производительность выросла до удивительных значений.
- *
- * Идея http://dev.1c-bitrix.ru/community/webdev/user/23242/
- *
- * @use
- * 1/ add connection config in .settings_extra
- * 'connections' => ['value' => [
- *      'default' => $defaultSettings['connections']['value']['default'],
- *      'memcache' => [
- *              'className' => \Bitrix\Main\Data\MemcacheConnection::class,
- *              'port' => 0,
- *              'host' => 'unix:///home/bitrix/memcached.sock'
- *      ],
- * ]],
- * 2/ add need classes within autoloader
- *    (GlobalsCacher, StaticPropertiesCacher, ManagedCacheArrayWrapper,
- *     MemcacheWrapper, MemcacheNestedArrayWrapper, MemcacheWrapperError, UUtils)
- * 3/ require file in init.php
- * require __DIR__ . '/include/ib_props_memcache.php';
- *
- * !!! Обязательно перезапускать memcached при изменении инфоблоков в режим хранения свойств 2.0 и обратно
- *
- * 4/ TODO need patch file bitrix/modules/iblock/classes/general/iblockproperty.php
- * bitrix is_set function to isset-construction
- *
- * @version 1.5.2
- * @author hipot, 2022
+ * @see https://dev.1c-bitrix.ru/community/webdev/user/17890/blog/8910/?commentId=49110#com49110
+ * @see iblock 23.200.0 https://dev.1c-bitrix.ru/docs/versions.php?lang=ru&module=iblock
+ * @version 2.1
+ * @author hipot, 2026
  */
 defined('B_PROLOG_INCLUDED') || die();
 
@@ -86,6 +20,7 @@ use Bitrix\Main\Loader,
 	Hipot\Services\BitrixEngine;
 
 (static function () {
+	// region CCatalogSku managed cache
 	try {
 		if (
 			class_exists(ManagedCacheArrayWrapper::class)
@@ -94,14 +29,14 @@ use Bitrix\Main\Loader,
 		) {
 			$managedCache = Application::getInstance()->getManagedCache();
 			$cacheTtl = 3600 * 24 * 30;
-			$cacheTableId = 'orm_b_catalog_iblock';
+			$cacheTableId = 'orm_hipot_b_catalog_iblock';
 			$wrapper = static fn(string $property): ManagedCacheArrayWrapper => new ManagedCacheArrayWrapper(
 				'hipot.ccatalogsku.' . $property . '.v1.',
 				$managedCache,
 				$cacheTtl,
 				$cacheTableId,
 			);
-
+			
 			(new StaticPropertiesCacher([
 				CCatalogSku::class => [
 					'arOfferCache' => static fn(): ManagedCacheArrayWrapper => $wrapper('offer'),
@@ -116,7 +51,9 @@ use Bitrix\Main\Loader,
 			UUtils::logException($exception);
 		}
 	}
-
+	// endregion
+	
+	// region Speed up \CIBlockProperty::GetPropertyArray and \CIblockElement::SetPropertyValues(Ex) wia memcache
 	if (
 		!class_exists('Memcache')
 		|| !class_exists(MemcacheWrapper::class)
@@ -131,34 +68,34 @@ use Bitrix\Main\Loader,
 		/** @var MemcacheConnection $mc */
 		$mc = Application::getConnection('memcache');
 		if (null !== $mc) {
-
+			
 			$namespaceProvider = static function (): string {
 				static $cacheNamespace = null;
 				if ($cacheNamespace !== null) {
 					return $cacheNamespace;
 				}
-
+				
 				// Keep caches for different iblock storage versions isolated.
 				$iblockVersions = [];
 				$result = IblockTable::query()
-					->setSelect(['ID', 'VERSION'])
-					->setOrder(['ID' => 'ASC'])
-					->setCacheTtl(3600 * 24 * 3)
-					->exec();
+				                     ->setSelect(['ID', 'VERSION'])
+				                     ->setOrder(['ID' => 'ASC'])
+				                     ->setCacheTtl(3600 * 24 * 3)
+				                     ->exec();
 				while ($iblock = $result->fetch()) {
 					$iblockVersions[$iblock['ID']] = $iblock['VERSION'];
 				}
-
+				
 				$serverName = (string)Application::getInstance()
-					->getContext()
-					->getServer()
-					->getServerName();
-
+				                                 ->getContext()
+				                                 ->getServer()
+				                                 ->getServerName();
+				
 				$cacheNamespace = md5(serialize($iblockVersions) . $serverName);
-
+				
 				return $cacheNamespace;
 			};
-
+			
 			(new GlobalsCacher(
 				$mc->getResource(),
 				[
@@ -185,15 +122,15 @@ use Bitrix\Main\Loader,
 					if (!$connection instanceof Memcache) {
 						throw new InvalidArgumentException('Memcache connection expected.');
 					}
-
+					
 					if (str_starts_with($prefix, 'BX_IBLOCK_PROP_CACHE_')) {
 						return new MemcacheNestedArrayWrapper($prefix, $connection);
 					}
-
+					
 					return new MemcacheWrapper($prefix, $connection);
 				},
 			))->cache();
-
+			
 			BitrixEngine::getInstance()->eventManager->addEventHandler(
 				'iblock',
 				'OnAfterIBlockPropertyDelete',
@@ -211,6 +148,7 @@ use Bitrix\Main\Loader,
 		UUtils::logException($e);
 	}
 	unset($mc);
+	// endregion
 	
 	// _tests:
 	/*
@@ -225,5 +163,5 @@ use Bitrix\Main\Loader,
 		exit;
 	}
 	*/
-
+	
 })();
